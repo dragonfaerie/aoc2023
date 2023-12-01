6c665cc47400766c576770221ef26df68187ea33
Hope Ross <hope.ross@daveramsey.com>
2023-12-01 09:43:09 -0600

    day 1 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,6 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val fileName = "testdata"
-    var tempList: MutableList<Int> = ArrayList()
-    var holdWord: String = ""
-
-    var acc = 0
-    File(fileName).forEachLine {
-        it.map{ c ->
-            if (c.isLetter()) {
-                holdWord += c
-                println(holdWord)
-            } else {
-                if (holdWord.isNotBlank()) {
-                    when(holdWord){
-                        "two" -> tempList.add(2)
-                        "nine" -> tempList.add(9)
-                    }
-                    holdWord = ""
-                }
-                tempList.add(c.digitToInt())
-            }
-        }
-        println(tempList)
-        var startNum = tempList.first()
-        val endNum = tempList.last()
-        startNum *= 10
-        acc += startNum + endNum
-        println(tempList)
-        println(acc)
-        tempList.clear()
-        println(tempList)
-    }
-
-    println(acc)
+    day1parta()
+    day1partb()
 }
--- src/main/kotlin/day1asolve.kt
+++ src/main/kotlin/day1asolve.kt
@@ -1,14 +1,13 @@
 import java.io.File
 
-fun main(args: Array<String>) {
-    val fileName = "testdata1a"
+fun day1parta() {
+    val fileName = "/Users/hope.ross/Desktop/mine/aoc2023/src/main/kotlin/day1testdata"
     var tempList: MutableList<Int> = ArrayList()
 
     var acc = 0
     File(fileName).forEachLine {
         it.map{ c ->
             if (c.isDigit()) {
-                println(c)
                 tempList.add(c.digitToInt())
             }
         }
@@ -16,10 +15,7 @@ fun main(args: Array<String>) {
         val endNum = tempList.last()
         startNum *= 10
         acc += startNum + endNum
-        println(tempList)
-        println(acc)
         tempList.clear()
-        println(tempList)
     }
 
     println(acc)
